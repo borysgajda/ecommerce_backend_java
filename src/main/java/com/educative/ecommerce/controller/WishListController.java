df52c881e01c5e338f230512b2a563c34a34ca3b
Borys Gajda <51117842+borysgajda@users.noreply.github.com>
2023-07-02 19:43:46 +0200

    Add files via upload

---
+++ src/main/java/com/educative/ecommerce/controller/WishListController.java
@@ -0,0 +1,64 @@
+package com.educative.ecommerce.controller;
+
+import com.educative.ecommerce.common.ApiResponse;
+import com.educative.ecommerce.dto.ProductDto;
+import com.educative.ecommerce.model.Product;
+import com.educative.ecommerce.model.User;
+import com.educative.ecommerce.model.WishList;
+import com.educative.ecommerce.service.AuthenticationService;
+import com.educative.ecommerce.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wishlist")
+public class WishListController {
+
+    @Autowired
+    WishListService wishListService;
+
+    @Autowired
+    AuthenticationService authenticationService;
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addToWishList(@RequestBody Product product,
+                                                     @RequestParam("token") String token) {
+        authenticationService.authenticate(token);
+
+        User user = authenticationService.getUser(token);
+
+        WishList wishList = new WishList(user, product);
+
+        wishListService.createWishlist(wishList);
+
+        ApiResponse apiResponse = new ApiResponse(true, "Added to wishlist");
+        return  new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/{token}")
+    public ResponseEntity<List<ProductDto>> getWishList(@PathVariable("token") String token) {
+
+        authenticationService.authenticate(token);
+
+        User user = authenticationService.getUser(token);
+
+        List<ProductDto> productDtos = wishListService.getWishListForUser(user);
+
+        return new ResponseEntity<>(productDtos, HttpStatus.OK);
+
+    }
+
+
+
+}
---
+++ src/main/java/com/educative/ecommerce/service/UserService.java
@@ -0,0 +1,91 @@
+package com.educative.ecommerce.service;
+
+import com.educative.ecommerce.dto.ResponseDto;
+import com.educative.ecommerce.dto.user.SignInDto;
+import com.educative.ecommerce.dto.user.SignInReponseDto;
+import com.educative.ecommerce.dto.user.SignupDto;
+import com.educative.ecommerce.exceptions.AuthenticationFailException;
+import com.educative.ecommerce.exceptions.CustomException;
+import com.educative.ecommerce.model.AuthenticationToken;
+import com.educative.ecommerce.model.User;
+import com.educative.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.datatype.DatatypeConstants;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationService authenticationService;
+
+    @Transactional
+    public ResponseDto signUp(SignupDto signupDto) {
+        if (Objects.nonNull(userRepository.findByEmail(signupDto.getEmail()))) {
+            throw new CustomException("user already present");
+        }
+
+        String encryptedpassword = signupDto.getPassword();
+
+        try {
+            encryptedpassword = hashPassword(signupDto.getPassword());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        User user = new User(signupDto.getFirstName(), signupDto.getLastName(),
+                signupDto.getEmail(), encryptedpassword);
+
+        userRepository.save(user);
+
+        final AuthenticationToken authenticationToken = new AuthenticationToken(user);
+
+        authenticationService.saveConfirmationToken(authenticationToken);
+
+        ResponseDto responseDto = new ResponseDto("success", "user created succesfully");
+        return responseDto;
+    }
+
+    private String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        String hash = DatatypeConverter
+                .printHexBinary(digest).toUpperCase();
+        return hash;
+    }
+
+    public SignInReponseDto signIn(SignInDto signInDto) {
+
+        User user = userRepository.findByEmail(signInDto.getEmail());
+
+        if (Objects.isNull(user)) {
+            throw new AuthenticationFailException("user is not valid");
+        }
+
+        try {
+            if (!user.getPasswoprd().equals(hashPassword(signInDto.getPassword()))) {
+                throw new AuthenticationFailException("wrong password");
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        AuthenticationToken token = authenticationService.getToken(user);
+
+        if (Objects.isNull(token)) {
+            throw new CustomException("token is not present");
+        }
+
+        return new SignInReponseDto("sucess", token.getToken());
+    }
+}
